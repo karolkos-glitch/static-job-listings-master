f018180df176088cb0f3a0b714103224b43caa3c
Karol <karol99kos@gmail.com>
2020-12-30 16:20:03 +0100

    now jobs filtering should work properly

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -7,12 +7,17 @@ const jobs_data : JOB[] = JSON.parse(JSON.stringify(jsonDATA));
 
 const Jobs = () => {
   const [activeFilters, setactiveFilters] = useState<string[]>([]);
-  const [jobs, setJobs] = useState<JOB[]>(jobs_data);
+  const [jobs, setJobs] = useState<JOB[]>([]);
+  const deleteFilter = (filter :string) => {
+    setactiveFilters(prevFilters => prevFilters.filter(el => el!== filter))
+  }
+  
   useEffect(() => {
    console.log("Job list has changed!")
+   setJobs(jobs_data);
    if(activeFilters.length !== 0){
      console.log("There are some filters!");
-     let newJobs : JOB[] = jobs;
+     let newJobs : JOB[] = jobs_data;
      for(let i=0; i < activeFilters.length; i++){
        let filterToApply = activeFilters[i];
        newJobs = newJobs.filter(job => {
@@ -22,13 +27,14 @@ const Jobs = () => {
      }
      setJobs(newJobs);
    }
+  //  eslint-disable-next-line react-hooks/exhaustive-deps
    }, [activeFilters])
    
   return (
     <Fragment>
       <ul>
         <li><strong>Active Filters:</strong></li>
-        {activeFilters.map(filter=> <li key={filter}>{filter}</li>)}
+        {activeFilters.map(filter=> <li key={filter}>{filter} <button onClick={()=>deleteFilter(filter)}>DELETE</button></li>)}
       </ul>
       {jobs.map(job => <Job key={job.id} activateFilters={setactiveFilters} {...job}/>)}
     </Fragment>
